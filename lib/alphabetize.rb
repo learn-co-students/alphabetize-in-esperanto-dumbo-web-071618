d1aa73e365b1c1d6e29831e96f7f9bf047384896
Bryan Teng <bteng1226@gmail.com>
2018-07-01 21:59:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   eSPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word| 
-    word.split(" ").map do |letter|
+    word.split("").map do |letter|
       eSPERANTO_ALPHABET.index(letter)
     end
   end
