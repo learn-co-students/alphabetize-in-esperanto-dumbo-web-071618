1f13cc2853baef331c923b4a1c4ebd7a2b803b90
Josue V. Alorro <joalorro@gmail.com>
2018-07-14 04:02:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
+array = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by! do |phrase|
+    esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+    a = []
+    
+    mod = phrase.delete ' '
+    mod.each_char do |c|
+      a << esp_alphabet.index(c)
+    end
+    a
+  end
+  arr
 end
+
+puts alphabetize array 
